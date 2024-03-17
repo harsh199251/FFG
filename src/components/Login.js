b4f076eebb8997c2bd45b348682c6dcfcd00ef1a
harsh199251 <harshjain49251@gmail.com>
2024-03-17 14:43:26 +0530

    new12

--- src/components/Login.js
+++ src/components/Login.js
@@ -17,7 +17,7 @@ export default function Login() {
         e.preventDefault();
     
         try {
-          const response = await fetch('http://localhost:5000/api/admin-login', {
+          const response = await fetch('https://ffg-z4nr.onrender.com/api/admin-login', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
